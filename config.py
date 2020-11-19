84fd35e6af41a38fa004b5d22e2019424c6ec76b
Kouseki <30703483+Katou-Kouseki@users.noreply.github.com>
2020-11-19 10:58:17 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,8 @@
+ICON_RES = 'ys'  # res/img下的目录名
+ICON_PREFIX = 'Icon_No.'  # 图片编号前缀
+ID_OFFSET = 0  # 图片编号与data编号的差值
+ICON_EXT = 'png'  # 图片格式
+
+START_COMMAND = ('猜头像', '猜角色', '原神猜')  # 游戏开始命令
+RANK_COMMAND = ('猜头像排行榜', '猜角色排行榜', '原神猜排行榜', '排行榜')  # 排行榜命令
+DB_PATH = r'~/.hoshino/winning_counter.db'  # 排行榜数据存放位置
